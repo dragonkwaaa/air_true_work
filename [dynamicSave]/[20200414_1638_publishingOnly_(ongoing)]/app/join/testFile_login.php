2610dc2830fd0b768a4f495de6ea2d28d1f36eeb
dragonkwaaa <61732954+dragonkwaaa@users.noreply.github.com>
2020-04-14 17:13:36 +0900

    Create testFile_login.php

---
+++ [dynamicSave]/[20200414_1638_publishingOnly_(ongoing)]/app/join/testFile_login.php
@@ -0,0 +1,31 @@
+<?php include $_SERVER['DOCUMENT_ROOT'] . '/app/common/page/testFile_head.php'; ?>
+<div class="container">
+	<div class="contents">
+		<section class="login">
+			<h1 class="logo f_main">
+				<i class="logoIcon"></i>
+			</h1>
+			<div class="loginGroup">
+				<div class="inputBox">
+					<input class="tbox full big" placeholder="아이디">
+				</div>
+				<div class="inputBox">
+					<input class="tbox full big" placeholder="비밀번호">
+				</div>
+				<div class="inputBox">
+					<a href="main.html" class="btn full big col_purple f_w">로그인</a>
+				</div>
+				<ul class="subBox">
+					<li>아이디찾기</li>
+					<li>비밀번호 찾기</li>
+					<li>회원가입</li>
+				</ul>
+			</div>
+		</section>
+	</div>
+</div>
+
+<script>
+</script>
+</body>
+</html>
