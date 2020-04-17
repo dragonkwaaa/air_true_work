36c75ddaa411f7f7ab1e6f6298f1eed3f015dc60
dragonkwaaa <61732954+dragonkwaaa@users.noreply.github.com>
2020-04-17 16:02:36 +0900

    Create testFile_index.php

---
+++ [dynamicSave]/[20200414_1638_publishingOnly_(ongoing)]/app/school/testFile_index.php
@@ -0,0 +1,315 @@
+<?php include $_SERVER['DOCUMENT_ROOT'] . '/app/common/page/testFile_head.php';
+
+$pageName			=	'성산중학교';
+
+?>
+<div class="container monoBG">
+	<?php include $_SERVER['DOCUMENT_ROOT'] . '/app/common/page/testFile_header.php'; ?>
+
+	<div class="contents">
+		<section class="schoolState underSpaceMod">
+			<div class="schoolStateList">
+				<div class="schoolClassBox">
+					<div class="classTitle">
+						<a href="#none">1학년</a>
+					</div>
+					<div class="classInfo">
+						<div class="classList">
+							<div class="classStateBox good">
+								<div class="classState">
+									<a href="javascript:void(0)"></a>
+									<span class="className">1반</span>
+									<i class="stateIcon"></i>
+									<span class="stateText">좋음</span>
+									<span class="stateType">이산화탄소</span>
+									<span class="measure">420 PPM</span>
+								</div>
+							</div>
+							<div class="classStateBox normal">
+								<div class="classState">
+									<a href="javascript:void(0)"></a>
+									<span class="className">2반</span>
+									<i class="stateIcon"></i>
+									<span class="stateText">보통</span>
+									<span class="stateType">이산화탄소</span>
+									<span class="measure">420 PPM</span>
+								</div>
+							</div>
+							<div class="classStateBox bad">
+								<div class="classState">
+									<a href="javascript:void(0)"></a>
+									<span class="className">3반</span>
+									<i class="stateIcon"></i>
+									<span class="stateText">나쁨</span>
+									<span class="stateType">이산화탄소</span>
+									<span class="measure">420 PPM</span>
+								</div>
+							</div>
+							<div class="classStateBox worst">
+								<div class="classState">
+									<a href="javascript:void(0)"></a>
+									<span class="className">4반</span>
+									<i class="stateIcon"></i>
+									<span class="stateText">매우나쁨</span>
+									<span class="stateType">이산화탄소</span>
+									<span class="measure">420 PPM</span>
+								</div>
+							</div>
+							<div class="classStateBox worst">
+								<div class="classState">
+									<a href="javascript:void(0)"></a>
+									<span class="className">4반</span>
+									<i class="stateIcon"></i>
+									<span class="stateText">매우나쁨</span>
+									<span class="stateType">이산화탄소</span>
+									<span class="measure">420 PPM</span>
+								</div>
+							</div>
+							<div class="classStateBox worst">
+								<div class="classState">
+									<a href="javascript:void(0)"></a>
+									<span class="className">4반</span>
+									<i class="stateIcon"></i>
+									<span class="stateText">매우나쁨</span>
+									<span class="stateType">이산화탄소</span>
+									<span class="measure">420 PPM</span>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="schoolClassBox">
+					<div class="classTitle">
+						<a href="#none">2학년</a>
+					</div>
+					<div class="classInfo">
+						<div class="classList">
+							<div class="classStateBox good">
+								<div class="classState">
+									<a href="javascript:void(0)"></a>
+									<span class="className">1반</span>
+									<i class="stateIcon"></i>
+									<span class="stateText">좋음</span>
+									<span class="stateType">이산화탄소</span>
+									<span class="measure">420 PPM</span>
+								</div>
+							</div>
+							<div class="classStateBox normal">
+								<div class="classState">
+									<a href="javascript:void(0)"></a>
+									<span class="className">2반</span>
+									<i class="stateIcon"></i>
+									<span class="stateText">보통</span>
+									<span class="stateType">이산화탄소</span>
+									<span class="measure">420 PPM</span>
+								</div>
+							</div>
+							<div class="classStateBox bad">
+								<div class="classState">
+									<a href="javascript:void(0)"></a>
+									<span class="className">3반</span>
+									<i class="stateIcon"></i>
+									<span class="stateText">나쁨</span>
+									<span class="stateType">이산화탄소</span>
+									<span class="measure">420 PPM</span>
+								</div>
+							</div>
+							<div class="classStateBox worst">
+								<div class="classState">
+									<a href="javascript:void(0)"></a>
+									<span class="className">4반</span>
+									<i class="stateIcon"></i>
+									<span class="stateText">매우나쁨</span>
+									<span class="stateType">이산화탄소</span>
+									<span class="measure">420 PPM</span>
+								</div>
+							</div>
+							<div class="classStateBox worst">
+								<div class="classState">
+									<a href="javascript:void(0)"></a>
+									<span class="className">4반</span>
+									<i class="stateIcon"></i>
+									<span class="stateText">매우나쁨</span>
+									<span class="stateType">이산화탄소</span>
+									<span class="measure">420 PPM</span>
+								</div>
+							</div>
+							<div class="classStateBox worst">
+								<div class="classState">
+									<a href="javascript:void(0)"></a>
+									<span class="className">4반</span>
+									<i class="stateIcon"></i>
+									<span class="stateText">매우나쁨</span>
+									<span class="stateType">이산화탄소</span>
+									<span class="measure">420 PPM</span>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="schoolClassBox">
+					<div class="classTitle">
+						<a href="#none">3학년</a>
+					</div>
+					<div class="classInfo">
+						<div class="classList">
+							<div class="classStateBox good">
+								<div class="classState">
+									<a href="javascript:void(0)"></a>
+									<span class="className">1반</span>
+									<i class="stateIcon"></i>
+									<span class="stateText">좋음</span>
+									<span class="stateType">이산화탄소</span>
+									<span class="measure">420 PPM</span>
+								</div>
+							</div>
+							<div class="classStateBox normal">
+								<div class="classState">
+									<a href="javascript:void(0)"></a>
+									<span class="className">2반</span>
+									<i class="stateIcon"></i>
+									<span class="stateText">보통</span>
+									<span class="stateType">이산화탄소</span>
+									<span class="measure">420 PPM</span>
+								</div>
+							</div>
+							<div class="classStateBox bad">
+								<div class="classState">
+									<a href="javascript:void(0)"></a>
+									<span class="className">3반</span>
+									<i class="stateIcon"></i>
+									<span class="stateText">나쁨</span>
+									<span class="stateType">이산화탄소</span>
+									<span class="measure">420 PPM</span>
+								</div>
+							</div>
+							<div class="classStateBox worst">
+								<div class="classState">
+									<a href="javascript:void(0)"></a>
+									<span class="className">4반</span>
+									<i class="stateIcon"></i>
+									<span class="stateText">매우나쁨</span>
+									<span class="stateType">이산화탄소</span>
+									<span class="measure">420 PPM</span>
+								</div>
+							</div>
+							<div class="classStateBox worst">
+								<div class="classState">
+									<a href="javascript:void(0)"></a>
+									<span class="className">4반</span>
+									<i class="stateIcon"></i>
+									<span class="stateText">매우나쁨</span>
+									<span class="stateType">이산화탄소</span>
+									<span class="measure">420 PPM</span>
+								</div>
+							</div>
+							<div class="classStateBox worst">
+								<div class="classState">
+									<a href="javascript:void(0)"></a>
+									<span class="className">4반</span>
+									<i class="stateIcon"></i>
+									<span class="stateText">매우나쁨</span>
+									<span class="stateType">이산화탄소</span>
+									<span class="measure">420 PPM</span>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="schoolClassBox">
+					<div class="classTitle">
+						<a href="#none">시설물</a>
+					</div>
+					<div class="classInfo">
+						<div class="classList">
+							<div class="classStateBox good">
+								<div class="classState">
+									<a href="javascript:void(0)"></a>
+									<span class="className">1반</span>
+									<i class="stateIcon"></i>
+									<span class="stateText">좋음</span>
+									<span class="stateType">이산화탄소</span>
+									<span class="measure">420 PPM</span>
+								</div>
+							</div>
+							<div class="classStateBox normal">
+								<div class="classState">
+									<a href="javascript:void(0)"></a>
+									<span class="className">2반</span>
+									<i class="stateIcon"></i>
+									<span class="stateText">보통</span>
+									<span class="stateType">이산화탄소</span>
+									<span class="measure">420 PPM</span>
+								</div>
+							</div>
+							<div class="classStateBox bad">
+								<div class="classState">
+									<a href="javascript:void(0)"></a>
+									<span class="className">3반</span>
+									<i class="stateIcon"></i>
+									<span class="stateText">나쁨</span>
+									<span class="stateType">이산화탄소</span>
+									<span class="measure">420 PPM</span>
+								</div>
+							</div>
+							<div class="classStateBox worst">
+								<div class="classState">
+									<a href="javascript:void(0)"></a>
+									<span class="className">4반</span>
+									<i class="stateIcon"></i>
+									<span class="stateText">매우나쁨</span>
+									<span class="stateType">이산화탄소</span>
+									<span class="measure">420 PPM</span>
+								</div>
+							</div>
+							<div class="classStateBox worst">
+								<div class="classState">
+									<a href="javascript:void(0)"></a>
+									<span class="className">4반</span>
+									<i class="stateIcon"></i>
+									<span class="stateText">매우나쁨</span>
+									<span class="stateType">이산화탄소</span>
+									<span class="measure">420 PPM</span>
+								</div>
+							</div>
+							<div class="classStateBox worst">
+								<div class="classState">
+									<a href="javascript:void(0)"></a>
+									<span class="className">4반</span>
+									<i class="stateIcon"></i>
+									<span class="stateText">매우나쁨</span>
+									<span class="stateType">이산화탄소</span>
+									<span class="measure">420 PPM</span>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+	</div>
+	<div class="csCallBox fixedMod">
+		<a href="javascript:callNumber('1644-1224')" class="csCallLink">
+			<img class="csCallIcon" src="/app/common/img/icon_csCall_alt2.png">
+			<span class="csCallNumber">1644-1224</span>
+		</a>
+		<div class="csCallInfo">
+			<div class="csTime">
+				<pre>
+업무시간 - 09:00 ~ 18:00 (점심시간 12:00~13:00)
+휴무 - 토요일, 일요일, 공휴일
+				</pre>
+			</div>
+		</div>
+	</div>
+	<?php include $_SERVER['DOCUMENT_ROOT'] . '/app/common/page/testFile_footer.php'; ?>
+</div>
+
+<script>
+	$('.classTitle a').on('click',function () {
+
+		$(this).parents('.schoolClassBox').toggleClass('on');
+	});
+</script>
+</body>
+</html>
